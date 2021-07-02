580eb5ff3c8b41303e548bcd19cfbcc90531dacb
SailorMoonCrystal <kristinaa179@gmail.com>
2021-07-02 18:33:14 +0200

    oauth github commented, oauth google on root

--- app.py
+++ app.py
@@ -2,14 +2,54 @@ from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import ContactForm, GuestBookForm
 from datetime import datetime
 from azure.AzureDB import AzureDB
+from flask_dance.contrib.github import make_github_blueprint, github
+from flask_dance.contrib.google import make_google_blueprint, google
+import secrets
+import os
 
 
 app = Flask(__name__)
-app.secret_key = "secretKey"
+app.secret_key = secrets.token_hex(16)
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+github_blueprint = make_github_blueprint(
+    client_id="b419e4c63deab1368452",
+    client_secret="bc59429dc0fd7176120827a0cddff7d03ec5cccb",
+)
+app.register_blueprint(github_blueprint, url_prefix="/login")
+
+blueprint = make_google_blueprint(
+    client_id="277463384730-picsgp2plur3gu24d2887roqcm4i8q8v.apps.googleusercontent.com",
+    client_secret="dqz6bb9NDPZBFVZqUAZGYcBZ",
+    scope=[
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "openid",
+        "https://www.googleapis.com/auth/userinfo.email",
+    ],
+)
+app.register_blueprint(blueprint, url_prefix="/login")
+# @app.route("/")
+# def index():
+#     return render_template("carousel.html")
+
+
+# @app.route("/")
+# def index():
+#     if not github.authorized:
+#         return redirect(url_for("github.login"))
+#     else:
+#         account_info = github.get("/user")
+#         if account_info.ok:
+#             account_info_json = account_info.json()
+#             return render_template("carousel.html")
+#     return "<h1>Request failed!</h1>"
 
 
 @app.route("/")
 def index():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    resp = google.get("/plus/v1/people/me")
     return render_template("carousel.html")
 
 
